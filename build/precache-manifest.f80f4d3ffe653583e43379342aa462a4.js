bf31d91ab8f0d52afb20369771773344f794fc04
Rohan Aggarwal <aggarwal.rohan17@gmail.com>
2020-03-01 20:04:30 +0400

    initialcommit

---
+++ build/precache-manifest.f80f4d3ffe653583e43379342aa462a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5d5009f6939425d24373c9df61c8503",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd82c6aa5dd82f289b25",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7c82a34f153a558c2554",
+    "url": "/static/js/2.5980eb20.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.5980eb20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd82c6aa5dd82f289b25",
+    "url": "/static/js/main.3b052fec.chunk.js"
+  },
+  {
+    "revision": "ccf1d8c0179706a077aa",
+    "url": "/static/js/runtime-main.3a02ae19.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
